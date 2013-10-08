3e2ee62c5d6067cb52794b140b52d40ebfb931c6
Juan Vilar <troindx@gmail.com>
2013-10-08 13:44:50 +0200

    fixed SEF URL

--- dependencies/Session/Session.php
+++ dependencies/Session/Session.php
@@ -3,7 +3,7 @@ class Session
 {
 	public function __construct()
 	{
-		Session_start();
+		session_start();
 		if (!isset($_SESSION['AUTH_TOKEN']) )
 		{
 			$_SESSION['AUTH_TOKEN'] = AUTH_TOKEN_DEFAULT;
@@ -12,7 +12,25 @@ class Session
 	
 	public function start()
 	{
-		Session_start();	
+		session_start();	
+	}
+
+	public function end()
+	{
+
+		// Unset all of the session variables.
+		$_SESSION = array();
+
+		// If it's desired to kill the session, also delete the session cookie.
+		// Note: This will destroy the session, and not just the session data!
+		if (ini_get("session.use_cookies")) 
+		{
+		    $params = session_get_cookie_params();
+		    setcookie(session_name(), '', 100);
+		}
+
+		// Finally, destroy the session.
+		session_destroy();
 	}
 	
 	public function get($key)
