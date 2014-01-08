e545f255f4fe1bca4a8116c65140330a445b4aa4
troindx <troindx@gmail.com>
2014-01-08 20:37:32 +0100

    Added GetView
    Added GetView to allow the insertion of different views

--- dependencies/ViewHelper/ViewHelper.php
+++ dependencies/ViewHelper/ViewHelper.php
@@ -8,26 +8,37 @@ class ViewHelper
 	protected $errors;
 	protected $translator;
 	protected $template;
+	protected $generated;
 
-	public function getView()
+	public function getView($controller)
 	{
-		return $this->view;
+		global $template, $action, $view, $name;
+		
+		//Guardamos el estado del framework
+		$tmpView = $view;
+		$tmpName = $name;
+		$tmpTemplate = $template;
+		$tmpAction = $action;
+		//Ejecutamos la acción invocando a su correspondiente controlador
+		$action = $controller;
+		$this->$controller();
+		//Restauramos el estado del framework a como si no se hubiese hecho nada
+		$action = $tmpAction;
+		$template = $tmpTemplate;
+		$view = $tmpView;
+		$name = $tmpName;
 	}
 
-	public function setView($v)
-	{
-		$this->view = $v;
-	}
-	
 	public function __construct()
 	{
 		$this->errors = new ErrorModule();
 		$this->translator = new Translator();
 		$this->translator->load(get_class($this));
-		$this->template = "default";
+		$this->template = TEMPLATE_DEFAULT;
 		$this->view = new stdClass;
 		global $ErrorModule;
 		$ErrorModule = $this->errors;
+		$generated = 0;
 	}
 	
 	function super() {
@@ -58,14 +69,18 @@ class ViewHelper
 			exit(10);
 			//TODO , this has to be made better on the next iteration
 		}
-		global $translator, $view, $name, $template, $actionView;
+		global $translator, $view, $name, $template, $actionView, $action;
 		$view = $this->view;
 		$translator = $this->translator;
 		$name = get_class($this);
 		$template = $this->getTemplate();
+		$this->generated = 1;
 		if (func_num_args() == 0)
 		{
-			$actionView = $name."View";
+			//Quitamos el "Controller" del nombre.
+			//Más rápido que str_replace
+			$tmp = substr($action,0, -10);
+			$actionView = $tmp."View";
 		}
 		else
 		{
@@ -73,6 +88,11 @@ class ViewHelper
 		}
 		include("templates/$template/$template.php");
 		
+	}
+
+	public function hasBeenGenerated()
+	{
+		return $this->generated;
 	}
 
 	public function preEvent()
